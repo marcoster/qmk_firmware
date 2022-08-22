c1f40fa9b106bfa4f1396d21cda855866bdfa320
Marco Sterbik <madbadmax00@gmail.com>
2022-08-22 23:04:19 +0200

    sofle: add keymap 'marcoster'

---
+++ keyboards/sofle/keymaps/marcoster/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define TAPPING_FORCE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+//#define TAPPING_TERM 170
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
