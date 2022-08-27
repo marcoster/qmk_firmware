d8ea42bbf1060bf9891f32c2b9093457891238e9
Marco Sterbik <madbadmax00@gmail.com>
2022-08-27 21:20:57 +0200

    sofle/marcoster: enable WAIT_FOR_USB
    WAIT_FOR_USB fixes bootup issues when used on a rp2040 / kb2040

--- keyboards/sofle/keymaps/marcoster/config.h
+++ keyboards/sofle/keymaps/marcoster/config.h
@@ -3,6 +3,8 @@
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 //#define TAPPING_TERM 170
+//
+#define WAIT_FOR_USB 1
 
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
