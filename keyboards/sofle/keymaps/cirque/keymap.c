f775bdf688dd3c97b65539446a4d861176061e88
Marco Sterbik <madbadmax00@gmail.com>
2022-09-20 23:06:30 +0200

    sofle/marcoster/cirque: remove qk_boot from ctrl key; update readme

--- keyboards/sofle/keymaps/cirque/keymap.c
+++ keyboards/sofle/keymaps/cirque/keymap.c
@@ -66,16 +66,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,                             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_DEL,
   KC_LSFT,  MT_A,   MT_S,     MT_D,    MT_F,   KC_G,                             KC_H,    MT_J,    MT_K,    MT_L,    MT_SCLN, KC_QUOT,     // with homerow mods
   KC_LCTRL, KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,     KC_MUTE,       XXXXXXX, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                    //KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
-                    QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
+                    KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
+                    //QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
 ),
 [_QWERTY_NO_HOMEROW] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                             KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,                             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_DEL,
   KC_LSFT,  KC_A,   KC_S,     KC_D,    KC_F,    KC_G,                             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,     // no homerow mods
   KC_LCTRL, KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,     KC_MUTE,       XXXXXXX, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                    //KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
-                    QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
+                    KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
+                    //QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,      KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
 ),
 
 /*
@@ -98,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,     KC_F,    KC_P,    KC_G,                            KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
   KC_LSFT,  KC_A,   KC_R,     KC_S,    KC_T,    KC_D,                            KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LCTRL, KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,     KC_MUTE,      XXXXXXX, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                    //KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,     KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
-                    QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,     KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
+                    KC_LCTRL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,     KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTRL
+                    //QK_BOOT, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,     KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, QK_BOOT
 ),
 /*
  * GAME
--- keyboards/sofle/keymaps/cirque/readme.md
+++ keyboards/sofle/keymaps/cirque/readme.md
@@ -1,7 +1,12 @@
-# Sofle keymap from marcoster
+# Sofle keymap from marcoster with cirque trackpad
 
 This keymap allows has taken some symbol definitions from the klor keyboard and some personal preferences (layer keys on left side, space and enter on the right side).
 Also it uses homerow mods and has a separate qwerty layer without those.
+
+## Warnings
+This is configured to have the cirque trackpad mounted to the right half **AND** to use the right half as main.
+On the secondary / left side the default keymap had to be used somehow - this still needs investigation...
+And the secondary tap did not work initially (with the 35mm trackpad).
 
 ## compile for adafruit kb2040 as controler
 The following command compiles this for a kb2040 with qmk's **CONVERT_TO** feature (**ALLOW_WARNINGS** is somehow needed for me; never mentioned in documentation).
