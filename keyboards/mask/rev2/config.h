35e26ce08f69137ab38e60ff7b549f17fe724202
Marco Sterbik <madbadmax00@gmail.com>
2023-04-03 18:02:12 +0200

    mask: add mask-keyboards

---
+++ keyboards/mask/rev2/config.h
@@ -0,0 +1,36 @@
+/* Copyright 2023 Marco Sterbik (@marcoster)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
+
+// wiring of each half
+#define MATRIX_ROW_PINS \
+    { D4, B4, B3, F7, F5 }
+#define MATRIX_COL_PINS \
+    { B2, B6, B5, E6, D7, C6, B1, F6 }
+#define DIODE_DIRECTION COL2ROW
+
+#define TAPPING_TERM 100
+#define DEBOUNCE 5
+
+#define TAP_CODE_DELAY 10
+
+/* communication between sides */
+#define SOFT_SERIAL_PIN D2
