495668a2db98502790c079fc00aff533785da3bb
Marco Sterbik <madbadmax00@gmail.com>
2023-10-16 15:21:20 +0200

    mask: keymap marcoster - change from colemak to colemak-dh

--- keyboards/mask/keymaps/marcoster/keymap.c
+++ keyboards/mask/keymaps/marcoster/keymap.c
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_COLEMAK] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                            KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,   KC_Q,   KC_W,     KC_F,    KC_P,    KC_G,                            KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
-  KC_LSFT,  KC_A,   KC_R,     KC_S,    KC_T,    KC_D,                            KC_H,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LCTL, KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,     KC_MUTE,      XXXXXXX, KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                           KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,   KC_Q,   KC_W,     KC_F,    KC_P,    KC_B,                           KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,
+  KC_LSFT,  KC_A,   KC_R,     KC_S,    KC_T,    KC_G,                           KC_M,   KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LCTL,  KC_Z,   KC_X,     KC_C,    KC_D,    KC_V,     KC_MUTE,     XXXXXXX, KC_K,   KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                     KC_LCTL, KC_LALT, KC_LGUI, KC_LOWER, KC_RAISE,     KC_ENT,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL
 ),
 /*
@@ -187,7 +187,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _______,   _______,    _______, _______, _______,   _______, _______,   _______,    _______, _______
   )
 };
-  
+
 
 #ifdef OLED_ENABLE
 
@@ -464,4 +464,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //     debug_keyboard = true;
 //     debug_mouse    = false;
 // }
-
